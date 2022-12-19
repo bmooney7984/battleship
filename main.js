7c35200bb1639a138329cd4974c70808289b3e49
Brendan Mooney <bmooney7984@gmail.com>
2022-12-19 16:58:39 -0500

    Add board factory

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
+const shipList = (function() {
+    function typeToSize(type) {
+      let size
+
+      switch (type) {
+        case "patrol boat":
+          size = 2
+          break
+        case "submarine":
+          size = 3
+          break
+        case "cruiser":
+          size = 3
+          break
+        case "battleship":
+          size = 4
+          break
+        case "carrier":
+          size = 5
+          break
+      }
+
+      return size
+    }
+
+  return {typeToSize}
+})()
+
 function shipFactory(type) {
-  let size
-  switch (type) {
-    case "patrol boat":
-      size = 2
-      break
-    case "submarine":
-      size = 3
-      break
-    case "cruiser":
-      size = 3
-      break
-    case "battleship":
-      size = 4
-      break
-    case "carrier":
-      size = 5
-      break
-  }
+  let size = shipList.typeToSize(type)
 
   let timesHit = 0
 
@@ -35,4 +46,76 @@ function shipFactory(type) {
   return { type, size, timesHit, isSunk, hit }
 }
 
+function boardFactory(player) {
+  const squaresShot = []
+
+  const shipLocations = {}
+
+  function locate(shipType, startSquare, orientation) {
+    const size = shipList.typeToSize(shipType)
+    let squares = []
+
+    if (orientation == "horizontal") {
+      for (let i = 1; i <= size; i++) {  // for ith square taken up by ship, add coordinates of that square to the list
+        const columnNum = Number(startSquare.slice(1)) + (i - 1)
+        const columnString = columnNum.toString()
+        squares.push(startSquare[0] + columnString)
+      }
+    } else if (orientation == "vertical") {
+      for (let i = 1; i <= size; i++) {  // for ith square taken up by ship, add coordinates of that square to the list
+        const rowNum = startSquare[0].charCodeAt(0) + (i - 1)
+        const rowString = String.fromCharCode(rowNum)
+        squares.push(rowString + startSquare.slice(1))
+      }
+    }
+
+    return squares
+  }
+
+  function outOfBounds(squares) {
+    return squares.some(function(square) {
+      return square.slice(1) < 1 || square.slice(1) > 10 || !['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'].includes(square[0])
+    })
+  }
+
+  function overlap(squares) {
+    return squares.some((square) => {
+      const occupiedSquares = Object.values(this.shipLocations).flat()
+      return occupiedSquares.some(function(occupiedSquare) {
+        return square == occupiedSquare
+      })
+    })
+  }
+
+  function placeShip(shipType, startSquare, orientation) {
+    const squares = locate(shipType, startSquare, orientation)
+
+    if (outOfBounds(squares)) {
+      throw "Ship doesn't fit in chosen location"
+    }
+
+    if (overlap.call(this, squares)) {
+      throw "Two ships cannot occupy the same square"
+    }
+
+    this.shipLocations[shipType] = squares
+  }
+
+  function receiveShot(square) {
+    this.squaresShot.push(square)
+
+    for (let shipType in this.shipLocations) {
+      if (this.shipLocations[shipType].includes(square)) {
+        const hitShip = player.ships.find(function(ship) {
+          return ship.type == shipType
+        })
+        hitShip.hit()
+      }
+    }
+  }
+
+  return { squaresShot, shipLocations, placeShip, receiveShot }
+}
+
 exports.shipFactory = shipFactory
+exports.boardFactory = boardFactory
--- main.test.js
+++ main.test.js
@@ -42,3 +42,61 @@ describe("ship methods", () => {
     expect(sub.isSunk()).toBe(true)
   })
 })
+
+describe("board attributes initialized", () => {
+  test("squaresShot set to empty array", () => {
+    const board = main.boardFactory({})
+    expect(board.squaresShot).toStrictEqual([])
+  })
+
+  test("shipLocations set to empty object", () => {
+    const board = main.boardFactory({})
+    expect(board.shipLocations).toStrictEqual({})
+  })
+})
+
+describe("board methods", () => {
+  test("placeShip places horizontal ship", () => {
+    const board = main.boardFactory({})
+    board.placeShip("cruiser", "C3", "horizontal")
+    expect(board.shipLocations).toStrictEqual({cruiser: ["C3", "C4", "C5"]})
+  })
+
+  test("placeShip places vertical ship", () => {
+    const board = main.boardFactory({})
+    board.placeShip("cruiser", "B5", "vertical")
+    expect(board.shipLocations).toStrictEqual({cruiser: ["B5", "C5", "D5"]})
+  })
+
+  test("placeShip throws error when ship runs off board horizontally", () => {
+    const board = main.boardFactory({})
+    expect(() => board.placeShip("cruiser", "A10", "horizontal")).toThrow("Ship doesn't fit in chosen location")
+  })
+
+  test("placeShip throws error when ship runs off board vertically", () => {
+    const board = main.boardFactory({})
+    expect(() => board.placeShip("cruiser", "J1", "vertical")).toThrow("Ship doesn't fit in chosen location")
+  })
+
+  test("placeShip throws error when two ships placed in same square", () => {
+    const board = main.boardFactory({})
+    board.placeShip("cruiser", "D2", "horizontal")
+    expect(() => board.placeShip("patrol boat", "C4", "vertical")).toThrow("Two ships cannot occupy the same square")
+  })
+
+  test("receiveShot records shot", () => {
+    const board = main.boardFactory({})
+    board.receiveShot("B5")
+    expect(board.squaresShot).toStrictEqual(["B5"])
+  })
+
+  test("receiveShot sends message to hit ship", () => {
+    const mockHit = jest.fn()
+    const mockPlayer = {ships: [{type: "patrol boat", hit: mockHit}]}
+    const board = main.boardFactory(mockPlayer)
+    board.shipLocations = {"patrol boat": ["C5", "C6"]}
+
+    board.receiveShot("C6")
+    expect(mockHit).toHaveBeenCalled()
+  })
+})
